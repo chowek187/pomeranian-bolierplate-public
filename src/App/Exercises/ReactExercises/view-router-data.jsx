523adafaa95f75c5d1cca626f4bcd66451240a40
chowek187 <chowek187@gmail.com>
2023-08-24 19:41:09 +0200

    onclick

--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,6 +1,9 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
-
-export const blockRouterMetaData = [SubRouteExampleMetaData];
+import { ReactOnClickRouterterMetaData } from '../../Exercises/ReactOnClick/router-data';
+export const blockRouterMetaData = [
+  SubRouteExampleMetaData,
+  ReactOnClickRouterterMetaData,
+];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
