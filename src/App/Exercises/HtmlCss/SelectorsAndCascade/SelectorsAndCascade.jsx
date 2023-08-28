e47ab9701560a12c40ec9ab9c181efe90b4cddbc
chowek187 <chowek187@gmail.com>
2023-08-28 20:47:13 +0200

    seeornot

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/SelectorsAndCascade.jsx
@@ -0,0 +1,17 @@
+import './styles.css';
+
+export const SelectorsAndCascade = () => {
+  return (
+    <>
+      <div class="test">
+        <span>Pierwszy</span>
+        <span>Drugi</span>
+        <span>Trzeci</span>
+      </div>
+
+      <button className="hover">Hover</button>
+      <input type="text" className="firstname" />
+      <div className="pseudo">Pseudo</div>
+    </>
+  );
+};
---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//import { Exercise } from './index';
+import { SelectorsAndCascade } from './SelectorsAndCascade';
+
+export const htmlCssRouterMetaData = {
+  path: 'selectos-and-cascade',
+  date: '28-08-2023',
+  linkLabel: 'HTML & CSS - ćwiczenie',
+  blockNo: 1,
+  element: <SelectorsAndCascade />,
+  tags: ['selectors', 'class', 'attribute'],
+};
