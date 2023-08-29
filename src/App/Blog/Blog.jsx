39ca126f315d4babdaf373592caeb04ed5d799be
chowek187 <chowek187@gmail.com>
2023-08-29 18:19:13 +0200

    blog

---
+++ src/App/Blog/Blog.jsx
@@ -0,0 +1,23 @@
+export const Blog = () => {
+  return (
+    <div>
+      <div className="blog-date">30-11-2022</div>
+      <h1 className="blog-title">Why are we so nostalgic for the 1990s?</h1>
+      <p className="blog-text">
+        Pop culture used to define a generation, but it seems the cultural,
+        music and fashion trends of the 1990s have been recycled, and what
+        Generation X considered its rite of passage into adulthood, is being
+        discovered and claimed by fledgling grown-ups as their own.
+      </p>
+      <p2 className="blog-text">
+        There's been the resurgence of vinyl as the trend-setters choice of
+        music consumption rather than the ease os a digital download, and now
+        the hipsters have discovered the nostalgic sound.
+      </p2>
+
+      <div className="author">
+        <p className="author-name">Radosława Majdan</p>
+      </div>
+    </div>
+  );
+};
