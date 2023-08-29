e6b4dd719c5cf8ae4b5973d0e501ff6526642caa
chowek187 <chowek187@gmail.com>
2023-08-29 18:59:39 +0200

    textFundaments

--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { htmlCssRouterMetaData } from './SelectorsAndCascade/router-data';
+import { textFundamentsRouterMetaData } from './TextFundaments/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   htmlCssRouterMetaData,
+  textFundamentsRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
