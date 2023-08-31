7a323c49176c3e197e4fa326e88cd7029dc5bb09
chowek187 <chowek187@gmail.com>
2023-08-31 19:05:47 +0200

    Multimedia

---
+++ src/App/Exercises/HtmlCss/MediaFiles/router-data.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MediaFiles } from '.';
+
+export const MediaFilesMetaData = {
+  path: 'media-files',
+  date: '31-08-2023',
+  linkLabel: 'Pliki multimedialne',
+  element: <MediaFiles />,
+  tags: ['html', 'video', 'audio', 'iframe'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -5,6 +5,7 @@ import { CssAnimationsRouterMetaData } from './CssAnimations/router-data';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { ImageFilesRouterMetaData } from './ImageFiles/router-data';
+import { MediaFilesMetaData } from './MediaFiles/router-data';
 import { htmlCssRouterMetaData } from './SelectorsAndCascade/router-data';
 import { StandardTagsRouterMetaData } from './StandardTags/router-data';
 import { textFundamentsRouterMetaData } from './TextFundaments/router-data';
@@ -18,5 +19,6 @@ export const blockRouterMetaData = [
   ColorsRouterMetaData,
   ImageFilesRouterMetaData,
   backgroundsRouterMetaData,
+  MediaFilesMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
