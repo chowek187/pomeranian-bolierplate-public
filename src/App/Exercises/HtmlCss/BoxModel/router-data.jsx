aa72e51350854a6deab634e25ff1f1ca753a7c74
chowek187 <chowek187@gmail.com>
2023-08-31 20:31:35 +0200

    Multimedia

---
+++ src/App/Exercises/HtmlCss/BoxModel/router-data.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BoxModel } from '.';
+
+export const BoxModelMetaData = {
+  path: 'box-and-display',
+  date: '31-08-2023',
+  linkLabel: 'Html & CSS - box model oraz display',
+  element: <BoxModel />,
+  tags: [],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -1,11 +1,13 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { backgroundsRouterMetaData } from './Backgrounds/router-data';
+import { BoxModelMetaData } from './BoxModel/router-data';
 import { ColorsRouterMetaData } from './Colors/router-data';
 import { CssAnimationsRouterMetaData } from './CssAnimations/router-data';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { ImageFilesRouterMetaData } from './ImageFiles/router-data';
 import { MediaFilesMetaData } from './MediaFiles/router-data';
+
 import { htmlCssRouterMetaData } from './SelectorsAndCascade/router-data';
 import { StandardTagsRouterMetaData } from './StandardTags/router-data';
 import { textFundamentsRouterMetaData } from './TextFundaments/router-data';
@@ -19,6 +21,7 @@ export const blockRouterMetaData = [
   ColorsRouterMetaData,
   ImageFilesRouterMetaData,
   backgroundsRouterMetaData,
+  BoxModelMetaData,
   MediaFilesMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
